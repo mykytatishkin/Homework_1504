96b9c4a31df26084c4031a9b940b7510fe9b4fad
Mykyta Tishkin <87891550+GTeasera@users.noreply.github.com>
2023-04-22 16:30:34 +0300

    init

---
+++ DanceProgressBar/Form1.Designer.cs
@@ -0,0 +1,166 @@
+﻿namespace DanceProgressBar
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.panelProgressBars = new System.Windows.Forms.Panel();
+            this.txtNumProgressBars = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.tbPath = new System.Windows.Forms.TextBox();
+            this.tbWord = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(118, 415);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Task 1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // panelProgressBars
+            // 
+            this.panelProgressBars.Location = new System.Drawing.Point(12, 12);
+            this.panelProgressBars.Name = "panelProgressBars";
+            this.panelProgressBars.Size = new System.Drawing.Size(377, 397);
+            this.panelProgressBars.TabIndex = 2;
+            // 
+            // txtNumProgressBars
+            // 
+            this.txtNumProgressBars.Location = new System.Drawing.Point(12, 415);
+            this.txtNumProgressBars.Name = "txtNumProgressBars";
+            this.txtNumProgressBars.Size = new System.Drawing.Size(100, 23);
+            this.txtNumProgressBars.TabIndex = 3;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(199, 415);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "Task 2";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 15;
+            this.listBox1.Location = new System.Drawing.Point(280, 12);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(508, 394);
+            this.listBox1.TabIndex = 5;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(280, 415);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 6;
+            this.button3.Text = "Task 3";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(361, 415);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 23);
+            this.button4.TabIndex = 7;
+            this.button4.Text = "Task 4";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // tbPath
+            // 
+            this.tbPath.Location = new System.Drawing.Point(582, 416);
+            this.tbPath.Name = "tbPath";
+            this.tbPath.Size = new System.Drawing.Size(100, 23);
+            this.tbPath.TabIndex = 8;
+            // 
+            // tbWord
+            // 
+            this.tbWord.Location = new System.Drawing.Point(688, 416);
+            this.tbWord.Name = "tbWord";
+            this.tbWord.Size = new System.Drawing.Size(100, 23);
+            this.tbWord.TabIndex = 9;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label1.Location = new System.Drawing.Point(490, 418);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(86, 21);
+            this.label1.TabIndex = 10;
+            this.label1.Text = "Path & Word";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.tbWord);
+            this.Controls.Add(this.tbPath);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.txtNumProgressBars);
+            this.Controls.Add(this.panelProgressBars);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Button button1;
+        private Panel panelProgressBars;
+        private TextBox txtNumProgressBars;
+        private Button button2;
+        private ListBox listBox1;
+        private Button button3;
+        private Button button4;
+        private TextBox tbPath;
+        private TextBox tbWord;
+        private Label label1;
+    }
+}
---
+++ DanceProgressBar/Form1.cs
@@ -0,0 +1,182 @@
+﻿using System;
+using System.Drawing;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+using ProgressBar = System.Windows.Forms.ProgressBar;
+
+namespace DanceProgressBar
+{
+    public partial class Form1 : Form
+    {
+
+        public Form1()
+        {
+            InitializeComponent();
+            if(txtNumProgressBars == null)
+                button1.IsAccessible = false; button2.IsAccessible = false;
+        }
+
+
+        // TASK 1
+        private async void button1_Click(object sender, EventArgs e)
+        {
+            int numProgressBars = Convert.ToInt32(txtNumProgressBars.Text);
+
+            panelProgressBars.Controls.Clear();
+
+            ProgressBar[] progressBars = new ProgressBar[numProgressBars];
+
+            Random random = new Random();
+            for (int i = 0; i < numProgressBars; i++)
+            {
+                ProgressBar progressBar = new ProgressBar();
+                progressBar.Location = new Point(10, 10 + i * 30);
+                progressBar.Name = "progressBar" + (i + 1).ToString();
+                progressBar.Size = new Size(200, 23);
+                progressBar.TabIndex = i;
+                progressBar.Value = 0;
+                progressBar.ForeColor = Color.FromArgb(random.Next(256), random.Next(256), random.Next(256));
+                panelProgressBars.Controls.Add(progressBar);
+
+                progressBars[i] = progressBar;
+            }
+
+            Task[] updateTasks = new Task[numProgressBars];
+            for (int i = 0; i < numProgressBars; i++)
+            {
+                int progressBarIndex = i;
+                updateTasks[i] = Task.Run(() =>
+                {
+                    ProgressBar progressBar = progressBars[progressBarIndex];
+                    for (int value = random.Next(99); value <= 100; value++)
+                    {
+                        progressBar.Invoke(new Action(() =>
+                        {
+                            progressBar.Value = value;
+                        }));
+
+                        Task.Delay(50).Wait();
+                    }
+                });
+            }
+
+            await Task.WhenAll(updateTasks);
+        }
+        
+
+
+        // TASK 2, start without debug
+        private async void button2_Click(object sender, EventArgs e)
+        {
+            int numProgressBars = Convert.ToInt32(txtNumProgressBars.Text);
+
+            panelProgressBars.Controls.Clear();
+
+            ProgressBar[] progressBars = new ProgressBar[numProgressBars];
+
+            Random random = new Random();
+            for (int i = 0; i < numProgressBars; i++)
+            {
+                ProgressBar progressBar = new ProgressBar();
+                progressBar.Location = new Point(10, 10 + i * 30);
+                progressBar.Name = "progressBar" + (i + 1).ToString();
+                progressBar.Size = new Size(200, 23);
+                progressBar.TabIndex = i;
+                progressBar.Value = 0;
+                progressBar.ForeColor = Color.FromArgb(random.Next(256), random.Next(256), random.Next(256));
+                panelProgressBars.Controls.Add(progressBar);
+
+                progressBars[i] = progressBar;
+            }
+
+            Task[] updateTasks = new Task[numProgressBars];
+            for (int i = 0; i < numProgressBars; i++)
+            {
+                int progressBarIndex = i;
+                updateTasks[i] = Task.Run(() =>
+                {
+                    ProgressBar progressBar = progressBars[progressBarIndex];
+                    for (int value = 0; value <= 100; value += random.Next(10))
+                    {
+                        progressBar.Invoke(new Action(() =>
+                        {
+                            progressBar.Value = value;
+                        }));
+
+                        Task.Delay(50).Wait(); 
+                    }
+                    listBox1.Items.Add("ProgressBar " + progressBar.Name + " has finished.");
+
+                });
+
+            }
+            
+
+            await Task.WhenAll(updateTasks);
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            int n = Convert.ToInt32(txtNumProgressBars.Text);
+            if (n <= 1)
+                return;
+
+            int prev = 0;
+            int current = 1;
+            int fib = 0;
+
+            for (int i = 2; i <= n; i++)
+            {
+                fib = prev + current;
+                prev = current;
+                current = fib;
+
+                if (current <= n)
+                    listBox1.Items.Add("Fibbonachi num: " + fib);
+            }
+
+            
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            // Вводим путь к файлу и целевое слово
+            Console.WriteLine("Введите путь к файлу:");
+            string filePath = tbPath.Text;
+
+            Console.WriteLine("Введите целевое слово:");
+            string targetWord = tbWord.Text;
+
+            // Открываем файл для чтения
+            using (StreamReader reader = new StreamReader(filePath))
+            {
+                string line;
+                int lineNumber = 1;
+
+                // Читаем файл построчно
+                while ((line = reader.ReadLine()) != null)
+                {
+                    // Разделяем строку на слова
+                    string[] words = line.Split(' ', ',', '.', ';', ':', '-', '!', '?');
+
+                    // Проверяем каждое слово на совпадение с целевым словом
+                    foreach (string word in words)
+                    {
+                        if (word.Equals(targetWord, StringComparison.OrdinalIgnoreCase))
+                        {
+                            listBox1.Items.Add("Word \"" + targetWord + "\" was in a line " + lineNumber + ": " + line);
+                        }
+                    }
+
+                    lineNumber++;
+                }
+            }
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
